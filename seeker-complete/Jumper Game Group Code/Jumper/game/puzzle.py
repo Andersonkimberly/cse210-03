6c88912d9ca1c644406c8a9bfb712baf5ab43ad2
camigm1 <camigm1@gmail.com>
2022-02-08 18:03:28 -0700

    changes

--- seeker-complete/Jumper Game Group Code/Jumper/game/puzzle.py
+++ seeker-complete/Jumper Game Group Code/Jumper/game/puzzle.py
@@ -1,6 +1,6 @@
 import random
 
-# Camila
+#Camila
 #List of random words
 #Word lengths
 #Each word letter & index for each letter
@@ -22,29 +22,30 @@ class List:
         Args:
             self (Hider): An instance of Hider.
         """
-        self.list = []
-    
-    def get_list(self):
+        self._list = ["star","beach","countryside","meadow","rainforest","wilderness","flower"]
+        self._word = ""
+        self.random_word()
+    def random_word(self):
         """Gets a hint for the seeker.
-
+        
         Args:
             self (Hider): An instance of Hider.
         
         Returns:
-            string: A hint for the seeker.
-        """
-        self.list = ["star","beach","countryside","meadow","rainforest","wilderness","flower",]
+            string: A hint for the seeker."""
+        random_index= random.randint(0,7)
+        self._word= self._list[random_index]
 
-    def is_found(self):
+    def get_word(self):
         """Whether or not the hider has been found.
 
         Args:
-            self (Hider): An instance of Hider.
+            
             
         Returns:
-            boolean: True if the hider was found; false if otherwise.
-        """
-        return (self._distance[-1] == 0)
+            """
+        
+        return self._word
         
     def watch_seeker(self, seeker):
         """Watches the seeker by keeping track of how far away it is.
@@ -53,4 +54,4 @@ class List:
             self (Hider): An instance of Hider.
         """
         distance = abs(self._location - seeker.get_location())
-        self._distance.append(distance)
+        # self._distance.append(distance)
