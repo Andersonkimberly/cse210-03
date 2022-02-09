67172fa0480960f2ba3917b3d7da05473602b717
RykerSteg <Rykersteglich@gmail.com>
2022-02-08 21:56:03 -0700

    changes to director, puzzle and display

--- seeker-complete/Jumper Game Group Code/Jumper/game/puzzle.py
+++ seeker-complete/Jumper Game Group Code/Jumper/game/puzzle.py
@@ -33,28 +33,17 @@ class List:
         
         Returns:
             string: Word from the list"""
-        random_index= random.randint(0,7)
+        random_index= random.randint(0,6)
         self._word= self._list[random_index]
 
-    def word_to_list(self):
-        list_of_letters= list(self.word)
 
-    def get_word(self):
-        """Whether or not the hider has been found.
+    # def get_word(self):
+    #     """Whether or not the hider has been found.
 
-        Args:
+    #     Args:
             
             
-        Returns:
-            """
+    #     Returns:
+    #         """
         
-        return self._word
-        
-    def watch_seeker(self, seeker):
-        """Watches the seeker by keeping track of how far away it is.
-
-        Args:
-            self (Hider): An instance of Hider.
-        """
-        distance = abs(self._location - seeker.get_location())
-        # self._distance.append(distance)
+    #     return self._word
